4e62999a558efc43518a2f4a68f271b62911cdf9
Riley Burke <128649341+rjb489@users.noreply.github.com>
2024-12-06 17:42:06 -0700

    Delete BinaryTreeTest.h

--- BinaryTreeTest.h
+++
@@ -1,8 +0,0 @@
-#ifndef C_DATASTRUCTURES_BINARYTREETEST_H
-#define C_DATASTRUCTURES_BINARYTREETEST_H
-
-#include "../../../../ErrorsTestStruct.h"
-
-void binaryTreeUnitTest();
-
-#endif //C_DATASTRUCTURES_BINARYTREETEST_H
